9c63238b8fdd754011cbbef594c38865d8bc4c81
lanchitour <hanjianqiao@gmail.com>
2017-04-10 17:12:54 +0800

    improve: show message 20 by 20

--- IOSrome/Resources/H5/1.js
+++ IOSrome/Resources/H5/1.js
@@ -1,19 +1,47 @@
 var str = '<a href="ios:showDetail:'
 var end = '"><div class="fl mr20"><img src="icon03.png" alt=""></div><div class="fl"><p>'
 var end1 = '</p><span>立即查看</span></div></a>';
+var target;
+var nowLoad = 0;
+var uid;
+var passwd;
+function lastStage0(para){
+    sel = document.getElementById('pull2up');
+    sel.innerHTML = '点击加载更多';
+}
+function lastStage1(para){
+    sel = document.getElementById('pull2up');
+    sel.innerHTML = '已显示全部消息';
+    sel.onclick = '';
+}
 function callBack(html, url){
-	var target = document.getElementsByClassName('bill')[0];
 	var obj = eval('('+html+')');
 	var message = obj.message;
-	for(var i = 0; i < message.length; i++){
-	    var item = document.createElement('li');
-	    var att = document.createAttribute('class');
-	    att.value = 'bill01';
-	    item.setAttributeNode(att);
-	    item.innerHTML = str+message[i].id+end+message[i].title+end1;
-	    target.insertBefore(item, target.childNodes[0])
-	}
+    if(message.length > 0){
+        for(var i = message.length-1; i >= 0; i--){
+            var item = document.createElement('li');
+            var att = document.createAttribute('class');
+            att.value = 'bill01';
+            item.setAttributeNode(att);
+            item.innerHTML = str+message[i].id+end+message[i].title+end1;
+            target.appendChild(item);
+        }
+        LanJsBridge.callInMain("lastStage0", "");
+    }else{
+        LanJsBridge.callInMain("lastStage1", "");
+    }
 }
-function updateDisplay(userId, password, messageId){
-	LanJsBridge.getDataFromUrl("http://user.vsusvip.com:30000/list?userid="+userId+"&password="+password, "callBack")
+
+function updateDisplay(userId, password){
+    uid = userId;
+    passwd = password;
+    var limit = 20;
+    target = document.getElementById('messagelist');
+    LanJsBridge.getDataFromUrl("http://user.vsusvip.com:30000/list?userid="+userId+"&password="+password+"&offset="+nowLoad+"&limit="+limit, "callBack");
+}
+
+function loadMore(){
+    var limit = 20;
+    nowLoad = nowLoad+limit;
+    LanJsBridge.getDataFromUrl("http://user.vsusvip.com:30000/list?userid="+uid+"&password="+passwd+"&offset="+nowLoad+"&limit="+limit, "callBack");
 }
