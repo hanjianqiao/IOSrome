e5ecf9a9bedce90013aab3a215c42825471b1858
lanchitour <hanjianqiao@gmail.com>
2017-05-28 20:26:53 +0800

    added: copy specific token

--- IOSrome/lanjs_s.js
+++ IOSrome/lanjs_s.js
@@ -413,36 +413,47 @@ function doWork(srcUrl, showit){
 }
 
 function copyToken(any){
-    var tokens = document.getElementsByClassName("code-wrap-s");
-    var tokenl = document.getElementsByClassName("code-wrap-l");
     var ret = "";
-    for(var i = 0; i < tokens.length; i++){
-        var htmlText = tokens[i].innerHTML;
-        var startIndex = htmlText.indexOf("class=\"label\"");
-        while(htmlText[startIndex] != ">"){startIndex++;}
-        startIndex++;
-        var endIndex = startIndex;
-        while(htmlText[endIndex] != "<"){endIndex++;}
-        ret += htmlText.substring(startIndex, endIndex);
-
-        startIndex = htmlText.indexOf("value=");
-        while(htmlText[startIndex] != "\""){startIndex++;}
-        startIndex++;
-        endIndex = startIndex;
-        while(htmlText[endIndex] != "\""){endIndex++;}
-
-        ret += htmlText.substring(startIndex, endIndex);
-        ret += "\n"
-    }
-    for(var i = 0; i < tokenl.length; i++){
-        var htmlText = tokenl[i].innerHTML;
-        var startIndex = htmlText.indexOf("\"clipboard-target\"");
-        while(htmlText[startIndex] != ">"){startIndex++;}
-        startIndex++;
-        var endIndex = startIndex;
-        while(htmlText[endIndex] != "<"){endIndex++;}
-        ret += htmlText.substring(startIndex, endIndex);
-        ret += "\n"
+    if(location.href.startsWith('http://pub.alimama.com/myunion.htm')){
+        var holder = document.getElementsByClassName('tab-content')[0].children;
+        for(var i = 0; i < holder.length; i++){
+            var item = holder[i];
+            if(item.style.display === 'block'){
+                ret += item.getElementsByClassName('getcode-box')[0].getElementsByClassName('textarea')[0].innerHTML;
+                break;
+            }
+        }
+    }else if(location.href.startsWith('http://pub.alimama.com/promo/search')){
+        var tokens = document.getElementsByClassName("code-wrap-s");
+        var tokenl = document.getElementsByClassName("code-wrap-l");
+        for(var i = 0; i < tokens.length; i++){
+            var htmlText = tokens[i].innerHTML;
+            var startIndex = htmlText.indexOf("class=\"label\"");
+            while(htmlText[startIndex] != ">"){startIndex++;}
+            startIndex++;
+            var endIndex = startIndex;
+            while(htmlText[endIndex] != "<"){endIndex++;}
+            ret += htmlText.substring(startIndex, endIndex);
+            
+            startIndex = htmlText.indexOf("value=");
+            while(htmlText[startIndex] != "\""){startIndex++;}
+            startIndex++;
+            endIndex = startIndex;
+            while(htmlText[endIndex] != "\""){endIndex++;}
+            
+            ret += htmlText.substring(startIndex, endIndex);
+            ret += "\n"
+        }
+        for(var i = 0; i < tokenl.length; i++){
+            var htmlText = tokenl[i].innerHTML;
+            var startIndex = htmlText.indexOf("\"clipboard-target\"");
+            while(htmlText[startIndex] != ">"){startIndex++;}
+            startIndex++;
+            var endIndex = startIndex;
+            while(htmlText[endIndex] != "<"){endIndex++;}
+            ret += htmlText.substring(startIndex, endIndex);
+            ret += "\n"
+        }
     }
     return ret;
 }
